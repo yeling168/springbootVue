57043c54c831b7291f2874f93e7d2706d1b2371c
yeling168 <1018714592@qq.com>
2022-12-17 14:06:37 +0800

    增加hello接口，测试test.http

--- src/main/java/com/example/springboot01/controller/TestController.java
+++ src/main/java/com/example/springboot01/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.example.springboot01.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,10 @@ public class TestController {
     @RequestMapping("/hello")
     public String hello(){
         return "Hello world";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "Hello World! Post," + name;
     }
 }
