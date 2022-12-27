77770932d7486a03f7c26e57027682e6f1ea9906
yeling168 <1018714592@qq.com>
2022-12-27 16:23:28 +0800

    封装请求参数和返回参数

--- src/main/java/com/example/springboot01/controller/EbookController.java
+++ src/main/java/com/example/springboot01/controller/EbookController.java
@@ -1,8 +1,9 @@
 package com.example.springboot01.controller;
 
-import com.jiawa.wiki.resp.CommonResp;
-import com.example.springboot01.domain.Ebook;
+import com.example.springboot01.req.EbookReq;
+import com.example.springboot01.resp.EbookResp;
 import com.example.springboot01.service.EbookService;
+import com.jiawa.wiki.resp.CommonResp;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +18,9 @@ public class EbookController {
     private EbookService ebookService;
 
     @RequestMapping("/list")
-    public CommonResp list(){
-        CommonResp<List<Ebook>> resp = new CommonResp<>();
-        List<Ebook> list = ebookService.list();
+    public CommonResp list(EbookReq req){
+        CommonResp<List<EbookResp>> resp = new CommonResp<>();
+        List<EbookResp> list = ebookService.list(req);
         resp.setContent(list);
         return resp;
     }
